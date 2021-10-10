ae49e83eb2cdf18169f250a15f68b22c36f2c47a
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2021-10-10 22:53:21 +0300

    * added tests

---
+++ test_align.py
@@ -0,0 +1,9 @@
+
+from align_russian_text import TextHandler, WordHandler
+from align_russian_text import GRAMMATICAL_RULES, ALPHABET, VOWELS, CONSONANTS
+
+# ..
+
+
+class TestWordHandler:
+    pass
